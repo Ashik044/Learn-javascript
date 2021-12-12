6c4559cdf66c7f5c1401ce29a79d7a1653b280d7
Ashik044 <rezaashik044@gmail.com>
2021-12-12 20:35:40 +0600

    ok

--- (3) hoisting.js
+++ (3) hoisting.js
@@ -2,21 +2,32 @@
 // var vs let and const
 // function
 
-// var a;
-// console.log(a);
-// a = "bangladesh";
-// console.log(a);
+//--------------------------------------------------------------------
+// // this is for var
+console.log(`this is for var: ${a}`);
+var a;
+console.log(`this is for var: ${a}`);
+a = "bangladesh";
+console.log(`this is for var: ${a}`);
 
-//=============================================
+//----------------------------------------------------------------------
+// // this is for let
+// console.log(`this is for let: ${b}`);
+// let b;
+// console.log(`this is for let: ${b}`);
+// b = "bangladesh";
+// console.log(`this is for let: ${b}`);
+
+//===========================================================================
 // console.log(a);
 // let a = "bangladesh";
 
-//-----------
+//---------------------------------------------------------------------------
 
 // console.log(b);
 // var b = "ashik";
 
-//==================================================
+//===========================================================================
 // let LANGUAGE = "java";
 // let language = "javascript";
 
