878122bd03e9ca9560f9828b84d7c686eb83dd2e
Ashik044 <rezaashik044@gmail.com>
2021-12-13 19:22:57 +0600

    ok

---
+++ (6) data_type_others.js
@@ -0,0 +1,50 @@
+// Array
+var countries = ["Bangladesh", "USA", "UK"];
+console.log(countries);
+
+//===============================================
+var b = countries[0];
+console.log(b);
+
+//===============================================
+b = countries[0].length;
+console.log(b);
+
+//===============================================
+countries[countries.length] = "sewden";
+console.log(countries);
+
+//===============================================
+//push
+countries.push("india");
+console.log(countries);
+console.log(`this is push: ${countries}`);
+
+//===============================================
+countries.pop();
+console.log(countries);
+console.log(`this is pop: ${countries}`);
+
+//===============================================
+countries.shift()
+console.log(countries);
+console.log(`this is shift: ${countries}`);
+
+//===============================================
+countries.unshift("Bangladesh")
+console.log(countries);
+console.log(`this is unshift: ${countries}`);
+
+//===============================================
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
