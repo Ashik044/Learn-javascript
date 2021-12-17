432311c154eb8d589d745c31bf5a967c412f5840
Ashik044 <rezaashik044@gmail.com>
2021-12-17 21:59:05 +0600

    ok

---
+++ 10_control_flow.js
@@ -0,0 +1,20 @@
+// if (true) {
+//   console.log("If statement executed");
+// } else {
+//   console.log("If statement not executed");
+// }
+
+//=================================================
+var age = prompt();
+
+if (age >= 50) {
+  console.log("old");
+} else if (age < 50 && age >= 30) {
+  console.log("Under 50 but above or equal 30!");
+} else if (age < 30 && age >= 18) {
+  console.log("Under 30 but above or equal 18!");
+} else {
+  console.log("Under 18!");
+}
+
+//=================================================
