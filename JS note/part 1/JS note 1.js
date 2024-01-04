1a6e9648c3a9f8e6282315c50a96e1b7a65cbc8d
Ashik044 <rezaashik044@gmail.com>
2024-01-04 20:53:03 +0600

    ok

--- JS note/part 1/JS note 1.js
+++ JS note/part 1/JS note 1.js
@@ -1,6 +1,5 @@
 // 1/  scope, lexical scoping, hoisting, and closures:
 
-
 // Scope:-
 
 // Scope refers to the visibility of variables and functions in different parts of your code. In JavaScript, there are two main types of scope:
@@ -10,12 +9,12 @@
 // Local Scope: Variables defined within a function are local to that function and can only be accessed within that function.
 
 // Code Example:
-let globalVar = 'I am global'; // Global scope
+let globalVar = "I am global"; // Global scope
 
 function checkScope() {
-  let localVar = 'I am local'; // Local scope
+  let localVar = "I am local"; // Local scope
   console.log(globalVar); // Accessible
-  console.log(localVar);  // Accessible
+  console.log(localVar); // Accessible
 }
 
 checkScope();
@@ -26,11 +25,11 @@ console.log(localVar); // Unaccessible, will throw a ReferenceError
 
 // Lexical Scoping:-
 
-// Lexical Scoping (also known as static scoping) means that the scope of a variable is determined by its position in the source code. Inner functions have access to the variables of their outer functions.
+// Lexical Scoping (also known as static scoping) means that the scope of a variable is determined by its position in the source code. Inner functions have access to the variables of their outer functions. But outer functions do not have access to Inner functions
 
 // Code Example:
 function outerFunction() {
-  var outerVar = 'I am outside!';
+  var outerVar = "I am outside!";
 
   function innerFunction() {
     console.log(outerVar); // Inner function has access to outerVar
@@ -49,11 +48,11 @@ outerFunction();
 
 // Code Example:
 console.log(hoistedVar); // Outputs: undefined
-var hoistedVar = 'Hoisted!';
+var hoistedVar = "Hoisted!";
 
 hoistedFunction(); // Outputs: 'This function has been hoisted!'
 function hoistedFunction() {
-  console.log('This function has been hoisted!');
+  console.log("This function has been hoisted!");
 }
 
 // ----------------------------------------------------------------------------------------------------------
@@ -64,7 +63,7 @@ function hoistedFunction() {
 
 // Code Example:
 function outerFunction() {
-  let outerVar = 'I am from the outer scope';
+  let outerVar = "I am from the outer scope";
 
   function innerFunction() {
     console.log(outerVar); // Inner function has access to outerVar
@@ -75,7 +74,6 @@ function outerFunction() {
 
 let myClosure = outerFunction();
 myClosure(); // Even though outerFunction has finished execution, myClosure still has access to outerVar
-
 
 // ***
 // Effective Note:
@@ -94,15 +92,13 @@ myClosure(); // Even though outerFunction has finished execution, myClosure stil
 // https://ui.dev/ultimate-guide-to-execution-contexts-hoisting-scopes-and-closures-in-javascript
 // https://dev.to/midasxiv/scopes-hoisting-closures-tricky-questions-4c85
 
-
 // =================================================================================================
 
-// 2/  regular function, function expression, named function expression, Arrow function, anonymous function: 
-
+// 2/  regular function, function expression, named function expression, Arrow function, anonymous function:
 
 // Regular function:-
 
-// A regular function is declared with the function keyword and can be named or anonymous. A regular function has its own this binding and an arguments object that contains the parameters passed to it. A regular function can be hoisted, which means it can be called before it is defined. 
+// A regular function is declared with the function keyword and can be named or anonymous. A regular function has its own this binding and an arguments object that contains the parameters passed to it. A regular function can be hoisted, which means it can be called before it is defined.
 
 // Code example:
 
@@ -112,9 +108,9 @@ function add(x, y) {
 }
 
 // Anonymous regular function
-var subtract = function(x, y) {
+var subtract = function (x, y) {
   return x - y;
-}
+};
 
 // Hoisting example
 console.log(multiply(2, 3)); // 6
@@ -126,19 +122,19 @@ function multiply(x, y) {
 
 // Function expression:-
 
-// A function expression is a function that is assigned to a variable or a property of an object. A function expression can be named or anonymous, but the name is only visible within the function scope. A function expression is not hoisted, which means it cannot be called before it is defined. A function expression has the same properties as a regular function, such as this binding and arguments object. 
+// A function expression is a function that is assigned to a variable or a property of an object. A function expression can be named or anonymous, but the name is only visible within the function scope. A function expression is not hoisted, which means it cannot be called before it is defined. A function expression has the same properties as a regular function, such as this binding and arguments object.
 
 // Code example:
 
 // Named function expression
 var square = function sq(x) {
   return x * x;
-}
+};
 
 // Anonymous function expression
-var cube = function(x) {
+var cube = function (x) {
   return x * x * x;
-}
+};
 
 // Not hoisting example
 console.log(square(2)); // 4
@@ -148,7 +144,7 @@ console.log(cube(2)); // ReferenceError: cube is not defined
 
 // Named function expression:-
 
-//  A named function expression is a special case of a function expression, where the function has a name that is visible only within the function scope. A named function expression is useful for debugging and recursion purposes. 
+//  A named function expression is a special case of a function expression, where the function has a name that is visible only within the function scope. A named function expression is useful for debugging and recursion purposes.
 
 // Code example:
 
@@ -158,7 +154,7 @@ var factorial = function fact(n) {
     return 1;
   }
   return n * fact(n - 1); // Recursive call
-}
+};
 
 console.log(factorial(5)); // 120
 console.log(fact(5)); // ReferenceError: fact is not defined
@@ -175,41 +171,40 @@ console.log(fact(5)); // ReferenceError: fact is not defined
 var power = (x, y) => x ** y;
 
 // Equivalent function expression
-var power = function(x, y) {
+var power = function (x, y) {
   return x ** y;
-}
+};
 
 // Implicit return example
-var greet = name => `Hello, ${name}!`;
+var greet = (name) => `Hello, ${name}!`;
 
 // Explicit return example
 var sum = (x, y) => {
   let result = x + y;
   return result;
-}
+};
 
 // -----------------------------------------------------------------------------------------
 
 // Anonymous function:-
 
-//  An anonymous function is a function that does not have a name. An anonymous function can be a regular function, a function expression, or an arrow function. An anonymous function is often used as a callback or a closure. 
- 
+//  An anonymous function is a function that does not have a name. An anonymous function can be a regular function, a function expression, or an arrow function. An anonymous function is often used as a callback or a closure.
+
 //  Code example:
 
 // Anonymous regular function
-setTimeout(function() {
+setTimeout(function () {
   console.log("Hello, world!");
 }, 1000);
 
 // Anonymous function expression
 var numbers = [1, 2, 3, 4, 5];
-var doubles = numbers.map(function(x) {
+var doubles = numbers.map(function (x) {
   return x * 2;
 });
 
 // Anonymous arrow function
-var triples = numbers.map(x => x * 3);
-
+var triples = numbers.map((x) => x * 3);
 
 // ***
 // Effective Note:
@@ -224,11 +219,9 @@ var triples = numbers.map(x => x * 3);
 
 // Anonymous functions are functions without a name. They can be regular functions, function expressions, or arrow functions. They are often used as callbacks or closures.
 
-
 // Learn more:
 // https://www.freecodecamp.org/news/the-difference-between-arrow-functions-and-normal-functions/
 // https://dev.to/mathlete/anonymous-functions-vs-named-functions-vs-arrow-functions-57pm
 // https://dev.to/vandnakapoor19/the-difference-between-regular-functions-and-arrow-functions-j7d
 
 // ===================================================================================================================
-
